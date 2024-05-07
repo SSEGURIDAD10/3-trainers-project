b0273a23e2dc52aafc40eff550123fcee0a61b58
Villagran <luisbmx_haro@hotmail.com>
2024-05-07 15:50:38 -0600

    Add Royes.jsx /frontend/constants

--- trainers-pokemon-frontend/src/App.jsx
+++ trainers-pokemon-frontend/src/App.jsx
@@ -1,28 +1,14 @@
-// import { useState } from 'react'
 import './App.css'
-import Card from './../../trainers-pokemon-frontend/src/components/Card'
-import ListCard from './../../trainers-pokemon-frontend/src/components/ListCard'
+import Routes from './constants/Routes'
 import Menu from './../../trainers-pokemon-frontend/src/components/Menu'
-import SearchBar from './../../trainers-pokemon-frontend/src/components/SearchBar'
-import Home from './pages/Home'
-import DetailTrainer from './pages/DetailTrainer'
-import FormTrainers from './pages/FormTrainers'
-import NotFound from './pages/NotFound'
-
 
 function App() {
-  // const [count, setCount] = useState(0)
 
   return (
     <>
     <Menu />
-    <SearchBar />
-    <ListCard />
-    <Card />
-    <Home />
-    <DetailTrainer />
-    <FormTrainers />
-    <NotFound />
+    <Routes />
+
     </>
   )
 }
