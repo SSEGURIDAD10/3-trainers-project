7127d9351d48295b694547028daff003c862ddc5
Villagran <luisbmx_haro@hotmail.com>
2024-05-07 18:01:43 -0600

    import logo & update menucss

--- trainers-pokemon-frontend/src/components/Menu/index.jsx
+++ trainers-pokemon-frontend/src/components/Menu/index.jsx
@@ -1,10 +1,27 @@
 // rafce
-import React from 'react'
+import './index.css';
+import logo from '../../assets/img/logo.png';
+// import { a } from 'react-router-dom';
 
 const Menu = () => {
   return (
-    <div>Menu</div>
-  )
-}
+    <nav>
+      <div>
+        <img src={logo} alt="logo de trainers" className="logo-img" />
+      </div>
+      <ul className="nav-items">
+        <a href="/" className="nav-a">
+          Inicio
+        </a>
+        <a href="/new-trainer" className="nav-a">
+          Registro entrenador
+        </a>
+        <a href="/about" className="nav-a">
+          Acerca de nosotros
+        </a>
+      </ul>
+    </nav>
+  );
+};
 
-export default Menu
+export default Menu;
