951773813674c28a08db9708ad54380104bd6c1e
Villagran <luisbmx_haro@hotmail.com>
2024-05-09 12:43:45 -0600

    se agrego el return del api y se reemplazo por la api improvisada del home que quedo cometada

--- trainers-pokemon-frontend/src/api/Trainers/index.js
+++ trainers-pokemon-frontend/src/api/Trainers/index.js
@@ -4,6 +4,7 @@ const getTrainers = async() => {
     const request = await fetch(URL);
     const response = await request.json();
     console.log('Respuesta: ', response)
+    return response
 }
 
 export default getTrainers
--- trainers-pokemon-frontend/src/pages/Home/index.jsx
+++ trainers-pokemon-frontend/src/pages/Home/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import SearchBar from './../../components/SearchBar'
 import ListCard from './../../components/ListCard'
 import Card from './../../components/Card'
@@ -6,17 +6,49 @@ import getTrainers from '../../api/Trainers'
 
 const Home = () => {
 
-  const trainers = [
-    {id: 1, name: 'Ash', team: [], rank: '', region: ''},
-    {id: 2, name: 'Misty', team: [], rank: '', region: ''},
-    {id: 3, name: 'Brock', team: [], rank: '', region: ''}
-  ];
+  const [numero, setNumero] = useState(0);
+  const [trainers, setTrainers] = useState([]);
 
-  getTrainers();
+  // const trainers = [
+  //   {id: 1, name: 'Ash', team: [], rank: '', region: ''},
+  //   {id: 2, name: 'Misty', team: [], rank: '', region: ''},
+  //   {id: 3, name: 'Brock', team: [], rank: '', region: ''}
+  // ];
+
+  const getData = async() => {
+    setTrainers(await getTrainers());
+  }
+
+  const addNumber = () => {
+    let newNumero = numero + 1;
+    setNumero(newNumero)
+  }
+
+  // // 1.- Se ejecuta en cada render
+  // useEffect(() => {
+  //   console.log('useEffect sin condiciones');
+  // });
+
+  //   // 2.- Solo se ejecuta una vez
+    useEffect(() => {
+      console.log('useEffect con []');
+      getData();
+    }, []);
+
+  //   // 3.- Se ejecuta cuando la condicion 1 cambia de valor (o las N condiciones)
+  // useEffect(() => {
+  //   console.log('useEffect con [condition1, condition2]');
+  // }, [condition1, condition2]);
 
   return (
     <>
     <SearchBar />
+
+    <div className='counter-container'>
+      <button onClick={addNumber}>Incrementar</button>
+      <span>El numero es: {numero}</span>
+    </div>
+
     <ListCard>
       {
       trainers.map((element, index) => {
